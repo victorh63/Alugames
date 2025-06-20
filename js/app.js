4b2baa683fdd27d00cb6ce290aa8e121088f0344
Victor <vichugo251106@hotmail.com>
2025-06-20 18:20:33 -0300

    Criação das variaveis 'imagem' e 'botao'

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
 };
