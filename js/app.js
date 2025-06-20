3612d8bea64e38fa2ed89e679b0557830c11a7d2
Victor <vichugo251106@hotmail.com>
2025-06-20 18:23:57 -0300

    Criação da variavel 'nomeJogo'

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 };
