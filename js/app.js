b9ad6402f38f9a0a78cd9956f27b06005b2c9fb9
Victor <vichugo251106@hotmail.com>
2025-06-23 23:07:01 -0300

    Delete da variavel nomeJogo

--- js/app.js
+++ js/app.js
@@ -2,5 +2,4 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 };
