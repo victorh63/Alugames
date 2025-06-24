486bf5b1994d86ca74dd72c0120ef9096280e53f
Victor <vichugo251106@hotmail.com>
2025-06-24 00:06:56 -0300

    Correção L9

--- js/app.js
+++ js/app.js
@@ -6,6 +6,6 @@ function alterarStatus(id){
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
     } else {
-        imagem.classList.add;('dashboard__item__img--rented');
+        imagem.classList.add('dashboard__item__img--rented');
     };
 };
