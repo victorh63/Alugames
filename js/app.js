2581c6ec940763af2df9f2a18a0922819fabc56b
Victor <vichugo251106@hotmail.com>
2025-06-24 00:14:06 -0300

    Adicionando as mudanças nos botões

--- js/app.js
+++ js/app.js
@@ -5,7 +5,11 @@ function alterarStatus(id){
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
     } else {
         imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
     };
 };
